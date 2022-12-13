55d7dd5a427df2d5e75dab46c3d2ccad23c1a48c
Cody Liska <codyliska@gmail.com>
2022-12-12 19:58:23 -0700

    search term displays after user clicks their option to search for

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-$(function(){
+// BOOK SEARCH AREA //
+document.getElementById('dropdown-menu').addEventListener('click', searchTerm)
 
-  $(".dropdown-menu").on('click', 'li a', function(){
-    $(".btn:first-child").text($(this).text());
-    $(".btn:first-child").val($(this).text());
- });
-
-});
+function searchTerm(item){
+  console.log(item.target.innerHTML)
+  document.getElementById('navbarDropdown').innerText = item.target.innerHTML
+}
