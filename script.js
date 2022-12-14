b94f0a3d9f6f6a29b26a57e9646f9df29e1392ee
Cody Liska <codyliska@gmail.com>
2022-12-14 16:02:58 -0700

    created the addBook function. currently it will console log that data from the input form

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// BOOK SUBMIT //
+const form = document.querySelector('form').addEventListener('submit', addBook)
+
+function addBook(event){
+  event.preventDefault()
+  const formValues = new FormData(event.target)
+  const bookData = Object.fromEntries(formValues.entries())
+  console.log({bookData})
+}
+
 // BOOK SEARCH AREA //
 document.getElementById('dropdown-menu').addEventListener('click', searchTerm)
 
 function searchTerm(item){
-  console.log(item.target.innerHTML)
   document.getElementById('navbarDropdown').innerText = item.target.innerHTML
 }
