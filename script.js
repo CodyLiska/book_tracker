b554a9e7838dc3df9fc49fa9e60a1e47f93838de
Cody Liska <codyliska@gmail.com>
2022-12-15 22:28:36 -0700

    display updates with data from form values

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ function addBook(e){
   const bookData = Object.fromEntries(formValues.entries())
   console.log({bookData})
   document.getElementById('input-form').reset()
+
+  document.getElementById('dispTitle').innerText = bookData.title
+  document.getElementById('dateStarted').innerText = bookData.startDate
+  document.getElementById('dispDateFinished').innerText = bookData.finishDate
+  document.getElementById('dispAuthor').innerText = bookData.author
+  document.getElementById('dispPublisher').innerText = bookData.publisher
+  document.getElementById('dispYearPublished').innerText = bookData.yearPublished
+  document.getElementById('dispGenere').innerText = bookData.fictionGenere
+  document.getElementById('dispLocation').innerText = bookData.fromWhere
+  document.getElementById('dispMainCharacters').innerText = bookData.mainCharacters
+  document.getElementById('dispReview').innerText = bookData.myThoughts
+  
+  //TODO 
+  //  hide labels on display side until btn is clicked
 }
 
 // BOOK SEARCH AREA //
